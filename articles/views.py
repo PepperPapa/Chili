5dc3ebbcc673d1a8ea2e95831e2b502c26f23b60
pepperpapa <zhongxin2506@outlook.com>
2019-01-26 00:14:49 +0800

    web页面设计初步完成
    1.web主页展示全部文章页面初步完成。
    2.写文章页面设计初步完成。
    3.文章页面详情页设计初步完成。

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Article
+import re
 
 # Create your views here.
 def index(request):
     all_articles = Article.objects.all()
+    # TODO:zx 这里随着数据量增加会有性能问题，后续改成生成器模式
+    # 把content中的<> </>中的内容全部替换掉，取出一定长度的内容作为summary
+    for article in all_articles:
+        article.content = re.sub(r'<\/?[a-z]+>', '', article.content)   #替换html tag为空内容
+        article.content = re.sub(r'<\s+', ' ', article.content) #多个空格替换为一个空格
+        article.content = article.content[:60]
     return render(request, 'articles/index.html', {'all_articles': all_articles})
 
 def new(request):
